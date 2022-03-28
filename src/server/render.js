e62752943c3b37a0ed5328523a37717f7e71e1c0
黎育存 <94412108@stud.sju.edu.tw>
2022-03-28 16:44:42 +0800

    update helmet bug

--- src/server/render.js
+++ src/server/render.js
@@ -1,33 +1,43 @@
 /*
- *   Copyright (c) 2021 
+ *   Copyright (c) 2021
  *   All rights reserved.
  */
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { StaticRouter, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { Helmet } from "react-helmet";
-import serialize from "serialize-javascript";
+import { Helmet } from 'react-helmet';
+import serialize from 'serialize-javascript';
 import App from '../shared/app';
-import { determineUserLang, dir } from "../common/i18n";
+import { determineUserLang } from '../common/i18n';
 
 export default (req, store) => {
   const context = {};
-  const helmet = Helmet.renderStatic();
   const initialData = store.getState();
   const lang = determineUserLang(req.acceptsLanguages(), req.path);
   const content = renderToString(
     <Provider store={store}>
       <StaticRouter context={context} location={req.path} basename={`/${lang}`}>
-        <Route render= {() => <App lang={lang} />} />
+        <Route render={() => <App lang={lang} />} />
       </StaticRouter>
     </Provider>,
   );
-  return`
+
+  const helmet = Helmet.renderStatic();
+  const helmetToString = Object.keys(helmet)
+    .filter(key => helmet[key].toString() !== '')
+    .map(item => helmet[item].toString());
+
+  return (`
     <html>
       <head>
-        ${helmet.title.toString()}
-        ${helmet.meta.toString()}
+        ${helmetToString.join('\n')}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="format-detection" content="telephone=no,email=no" />
+        <meta name="apple-touch-fullscreen" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="stylesheet" type="text/css" href="../css/main.css" />
       </head>
       <body>
@@ -37,5 +47,5 @@ export default (req, store) => {
         <script>window.__initialData__ = ${serialize(initialData)}</script>
       </body>
     </html>
-  `;
+  `);
 };
--- src/shared/pages/Home.js
+++ src/shared/pages/Home.js
@@ -1,27 +1,32 @@
 /*
- *   Copyright (c) 2021 
+ *   Copyright (c) 2021
  *   All rights reserved.
  */
 import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Helmet } from "react-helmet";
-import { useIntl, FormattedMessage } from "react-intl";
+import { Helmet } from 'react-helmet';
+import { useIntl, FormattedMessage } from 'react-intl';
 
 // Actions
 import { homeAction } from '../actions/home';
 
-const Home = ({ dispatch, members=[] }) => {
-
+const Home = ({
+  dispatch,
+  members = [],
+}) => {
   const intl = useIntl();
 
   useEffect(() => {
-    dispatch( homeAction() );
-  },[]);
+    dispatch(homeAction());
+  }, []);
 
-  return(
+  return (
     <>
       <Helmet>
-        <title>123</title>
+        <title>test home title</title>
+        <meta name="description" content="test description" />
+        <meta name="keywords" content="test,test1,test2" />
       </Helmet>
       <FormattedMessage id="home.title" />
       <FormattedMessage
@@ -29,26 +34,30 @@ const Home = ({ dispatch, members=[] }) => {
         values={{ addedOn: '2020-01-01', aaaa: 'test' }}
       />
       {
-        members.map(item => {
-          return(
-            <div key={item.id.value}>
-              <div className="">{`${item.name.title}：${item.name.last} ${item.name.first}`}</div>
-            </div>
-          );
-        })
+        members.map(item => (
+          <div key={item.id.value}>
+            <div className="">{`${item.name.title}：${item.name.last} ${item.name.first}`}</div>
+          </div>
+        ))
       }
     </>
   );
-}
+};
 
-Home.loadData = (dispatch) => {
-  return dispatch(homeAction());
-}
+Home.loadData = dispatch => dispatch(homeAction());
 
-const mapStateToProps = state => {
-  return{
-    members: state.home.list
-  }
-}
+const mapStateToProps = state => ({
+  members: state.home.list,
+});
+
+Home.defaultProps = {
+  dispatch: () => {},
+  members: [],
+};
+
+Home.propTypes = {
+  dispatch: PropTypes.func,
+  members: PropTypes.arrayOf,
+};
 
 export default connect(mapStateToProps)(Home);
